a6b7c77ffdb08783b3e0de64bd374fdd172aba50
Teemu Loijas <teemu.loijas@tuni.fi>
2020-11-13 16:50:00 +0200

    Add object medium prio tests

--- test/object.test.js
+++ test/object.test.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const countBy = require('../lib/src/countBy.js').default;
+const get = require('../lib/src/get.js').default;
+const keys = require('../lib/src/keys.js').default;
 
-const assert = chai.assert;
 const expect = chai.expect;
 
 describe("Object Test Suite", () => {
@@ -14,6 +15,28 @@ describe("Object Test Suite", () => {
         });
         it("Should count by empty", () => {
             expect(countBy([], u => u.a)).to.eql({});
+        });
+    });
+    describe("Function: Get", () => {
+        const object = { 'a': [{ 'b': { 'c': 3 } }] };
+        it("Should count by number", () => {
+            expect(get(object, 'a[0].b.c')).to.equal(3);
+        });
+        it("Should count by string", () => {
+            expect(get(object, ['a', '0', 'b', 'c'])).to.equal(3);
+        });
+    });
+    describe("Function: Keys", () => {
+        function Foo() {
+            this.a = 1
+            this.b = 2
+        }
+        Foo.prototype.c = 3;
+        it("Should count by number", () => {
+            expect(keys(new Foo)).to.eql(['a', 'b']);
+        });
+        it("Should count by string", () => {
+            expect(keys('hi')).to.eql(['0', '1']);
         });
     });
 });
